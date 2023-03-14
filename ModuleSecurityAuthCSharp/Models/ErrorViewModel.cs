41dd5359497b376a93c0a2dfb07bd727d3c660be
Tim Leach <timleach635@gmail.com>
2023-03-14 15:23:33 +0000

    AM-466: Create from template

---
+++ ModuleSecurityAuthCSharp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ModuleSecurityAuthCSharp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
